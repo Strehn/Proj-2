aa56df3543c1789c0e1ea1b13472014b6471cc1a
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2022-10-28 19:03:27 -0600

    formatting

--- pq-linkedlist.c
+++ pq-linkedlist.c
@@ -1,23 +1,27 @@
+// noah rodgers, sydney petrehn, and james turne
+// code using linked list and insertion sort
+
 #include <stdlib.h>
- #include <stdio.h>
- #include <time.h>
- #include “pq.h”
+#include <stdio.h>
+#include <time.h>
+#include “pq.h”
+
  int main(int argc, char *argv[])
  {
- int i;
- const int n = 1000;
- double *v = malloc(sizeof(double)*n);
- pq *mypq;
- /* init */
- srand(time(NULL));
- mypq = pq_create();
- for (i = 0; i < n; i++) v[i] = drand48();
- /* begin sort */
- for (i = 0; i < n; i++) pq_push(mypq, v[i], (void*)v[i]);
- for (i = 0; i < n; i++) v[i] = pq_pop(mypq);
- /* end sort */
+    int i;
+    const int n = 1000;
+    double *v = malloc(sizeof(double)*n);
+    pq *mypq;
+    /* init */
+    srand(time(NULL));
+    mypq = pq_create();
+    for (i = 0; i < n; i++) v[i] = drand48();
+    /* begin sort */
+    for (i = 0; i < n; i++) pq_push(mypq, v[i], (void*)v[i]);
+    for (i = 0; i < n; i++) v[i] = pq_pop(mypq);
+    /* end sort */
 
- for (i = 0; i < n; i++) printf(“%g\n”, v[i]);
- free(v);
- return 0;
+    for (i = 0; i < n; i++) printf(“%g\n”, v[i]);
+    free(v);
+    return 0;
  }
