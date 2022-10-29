8c77469a0eba5eb1bc79a76db4a12507a385481b
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2022-10-28 19:10:27 -0600

    Given files upload

--- pq.h
+++ pq.h
@@ -1,86 +1,17 @@
-#include <limits.h>
-#include<stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-// A structure to represent a linked list of nodes
-struct Node {
-    int num;
-    struct Node* Previous;
-    struct Node* Next;
-};
+typedef struct pq pq;
 
-// Function to add an item to stack.  It increases top by 1
-// -------
+/* Allocates and initializes a new pq */
+pq* pq_create();
 
-// Change this to implement insertion sort
+/* Adds value to pq based on numerical order of key */
+void pq_push(pq *head, double key, void *value);
 
-// -------
-void push(int num, struct Node *head){
-    // we know head will never be 0 in this function
-    struct Node* temp = head; 
-    int last = 0;
-    // step through list and add num
-    while(temp->num <= num){ // as soon as temp->num > num stop
-        if(temp->num == num){
-            return; // do nothing
-        }
-        if(temp->Next == NULL){ // last node in list
-            last = 1;
-            break;
-        }
-        temp = temp->Next;
-    }
+/* Returns value from pq having the minimum key */
+void* pq_pop(pq *head);
 
-    struct Node *node = (struct Node*) malloc(sizeof(struct Node));
-    node->num = num;
-    if(last = 1){ // add to end of list
-        node->Next = NULL;
-        node->Previous = temp;
-        temp->Next = node;
-    }
-    else{
-        // inset between two nodes
-        node->Next = temp->Next;
-        node->Previous = temp;
-        temp->Next = node;
-    }
-}
+/* Deallocates (frees) pq. Shallow destruction,
+meaning nodes in the pq are not recursively freed. */
+void pq_destroy();
 
-// Function to remove an item from stack.  It decreases top by 1
-// -------
-
-// Change this to implement insertion sort
-
-// -------
-void pop(int num, struct Node *head)
-{
-    if(head == NULL){
-        // do nothing
-        return;
-    }
-    // temp nodes to step through list and remove num
-    struct Node* temp = head;
-    struct Node* previous = NULL;
-    // step through LL
-    while(temp->num != num){
-        // no match
-        if(temp->Next == NULL){
-            return;
-        }
-        else{
-            previous = temp;
-            temp = temp->Next;
-        }
-    }
-    // first node in list so head becomes next
-    if(temp == head){
-        head = head->Next;
-    }
-    else{
-        previous->Next = temp->Next;
-        // remove current node
-    }
-}
 
