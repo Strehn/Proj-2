bd0f8aaf82d6c2fcf1fa136d8d5e684247ad4efd
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2022-11-10 10:59:54 -0700

    working on heap

--- pq-miniheap.c
+++ pq-miniheap.c
@@ -1,10 +1,87 @@
 // noah rodgers, sydney petrehn, and james turner
-// code using linked list and insertion sort
+// code implementing a mini heap
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include “pq.h”
 
+//min heap
+struct Mheap{
+    int *arr; // array to hold the binary tree
+    int size;
+}
+
+/*       root   <- minimum num
+   parent    parent
+ node node  node node <- largest num\
+
+represented as an array
+[min/root   parent parent node node node node]
+
+root     = arr [0]
+current  = arr[i]
+parent   = arr[(i-1)/2]
+left c   = arr[(2*i)+1]
+right c  = arr[(2*i)+2]
+*/
+int parent(int i){
+    return (i-1)/2;
+}
+int leftc(int i){
+    return (2*i + 1);
+}
+int rightc(int i){
+    return (2*i + 2);
+}
+
+Mheap* insertHeap(Mheap *heap, int num){
+    heap->arr[heap->size - 1] = num;
+    int current = heap->size - 1;
+    int temp = 0;
+    while(current > 0 && 
+    heap->arr[parent(current)] > heap->arr[current]){
+        temp = heap->arr[parent(current)];
+        heap->arr[parent(current)] = heap->arr[current];
+        heap->arr[current] = temp;
+        current = parent(current);
+    }
+    return heap;
+}
+
+
+int main(int argc, char *argv[])
+{
+  int i;
+  const int n = 1000;
+  double *v = malloc(sizeof(double)*n);
+  double **p = malloc(sizeof(double*)*n);
+  Mheap *mypq;
+
+  /* init */
+  srand48(time(NULL));
+  mypq = pq_create();
+  for (i = 0; i < n; i++) {
+    v[i] = drand48();
+    p[i] = &v[i];
+  }
+
+  /* begin sort */
+  for (i = 0; i < n; i++) pq_push(mypq, v[i], (void*)p[i]);
+  
+  // insert miniheap conversion here
+
+  /* end sort */
+
+  for (i = 0; i < n; i++) {
+    if (p[i]) printf("%g\n", *p[i]);
+  }
+
+  pq_destroy(mypq);
+
+  free(v);
+  free(p);
+  return 0;
+}
 
 
