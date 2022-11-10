d669e73021735a965c2df80f131212a8c557b8b8
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2022-11-10 10:27:39 -0700

    linked list done

--- pq-linkedlist.c
+++ pq-linkedlist.c
@@ -52,3 +52,35 @@ void sInsert(pq** head, pq* newNode){// head = sl, new = current
         newNode->prev = current;
     }
 }
+
+int main(int argc, char *argv[])
+{
+  int i;
+  const int n = 1000;
+  double *v = malloc(sizeof(double)*n);
+  double **p = malloc(sizeof(double*)*n);
+  pq *mypq;
+
+  /* init */
+  srand48(time(NULL));
+  mypq = pq_create();
+  for (i = 0; i < n; i++) {
+    v[i] = drand48();
+    p[i] = &v[i];
+  }
+
+  /* begin sort */
+  for (i = 0; i < n; i++) pq_push(mypq, v[i], (void*)p[i]);
+  InsertionSort(mypq);
+  /* end sort */
+
+  for (i = 0; i < n; i++) {
+    if (p[i]) printf("%g\n", *p[i]);
+  }
+
+  pq_destroy(mypq);
+
+  free(v);
+  free(p);
+  return 0;
+}
