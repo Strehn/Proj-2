3c5f28794e045d45c0857fb87f4836db17b7cca9
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2022-11-15 17:50:15 -0700

    linked list rename

--- pq-linkedlist.c
+++ pq-linkedlist.c
@@ -1,34 +1,12 @@
-// noah rodgers, sydney petrehn, and james turner
-// code using a doubly linked list and insertion sort
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-//#include "pq.h"
-
-// function declaration
-void InsertionSort(pq *head);
-void sInsert(pq* head, pq* newNode);
+#include "pq.h"
 
 /* Allocates and initializes a new pq */
 pq* pq_create()
 {
   return (pq*)malloc(sizeof(pq));
-}
-
-/* Adds value to pq based on numerical order of key */
-void pq_push(pq *head, double key, void *value)
-{
-  //if (head && !(head->ptr)) head->ptr = value;
-}
-
-/* Returns value from pq having the minimum key */
-double pq_pop(pq *head)
-{
-  //void *p = head->ptr;
-  //head->ptr = NULL;
-  //return p->num;
-  return 1;
 }
 
 /* Deallocates (frees) pq. Shallow destruction,
@@ -51,24 +29,6 @@ void PrintLL(pq *head){
             temp = temp->Next; // move to next node down list
         }
     }
-}
-
-// doubly linked list sorted using insertion sort
-// inserts every node into a new sorted list (sl)
-// ----- This function can be deleted we didnt end up using it -----
-void InsertionSort(pq *head){
-    pq *sl = pq_create();
-    pq *current = pq_create();
-    current = head;
-    pq *next = pq_create();
-    // go through list and insert each into a new sorted list
-    while (current != NULL) {
-        next = current->Next;
-        current->Previous = current->Next = NULL;
-        sInsert(sl, current); // put node in sorted list
-        current = next;
-    }
-    head = sl; // new head pointer points to head of sl
 }
 
 void sInsert(pq* head, pq* newNode){// head = sl, new = current
@@ -95,4 +55,33 @@ void sInsert(pq* head, pq* newNode){// head = sl, new = current
         current->Next = newNode;
         newNode->Previous = current;
     }
+}
+
+
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  const int n = 1000;
+  double *v = malloc(sizeof(double)*n);
+  struct pq *mypq;
+  mypq = pq_create();
+    /* init */
+    srand(time(NULL));
+    for (i = 0; i < n; i++) {
+      v[i] = drand48();
+    }
+
+    for (i = 0; i < n; i++){
+      struct pq *newNode = pq_create();
+      newNode->num = v[i];
+      newNode->Next = NULL;
+      newNode->Previous = NULL;        // INSERTION SORT
+      sInsert(mypq, newNode);
+    } 
+    
+    PrintLL(mypq); // print the data in the list
+
+    free(v);
+    free(mypq);
+    return 0;
 }
